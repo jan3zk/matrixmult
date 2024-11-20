dcb339395de80fc4419d02f33028132f21dc587b
jan3zk <janez.krizzaj@gmail.com>
2024-11-20 14:38:00 +0100

    updated matrix sizes

--- matrix_multiply.sh
+++ matrix_multiply.sh
@@ -7,6 +7,6 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=128
 
-srun --ntasks=1 --nodes=1 --exclusive ./matrix_multiply 5000 A1 B1
+srun --ntasks=1 --nodes=1 --exclusive ./matrix_multiply 2500 A1 B1
 
 wait
