47989a1dbae062da736c3d36d86b1b9e04407db0
jan3zk <janez.krizzaj@gmail.com>
2024-11-20 14:27:12 +0100

    updated reservation name, number of cpus-per-task

--- matrix_multiply.sh
+++ matrix_multiply.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=128
 
-srun --ntasks=1 --nodes=1 --exclusive ./matrix_multiply 2500 A1 B1
+srun --ntasks=1 --nodes=1 --exclusive ./matrix_multiply 5000 A1 B1
 
 wait
